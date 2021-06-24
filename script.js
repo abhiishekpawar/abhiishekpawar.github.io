01db20b04f8f086d4b5d0ab28b6d6372b6587d12
Abhishek Pawar <77561736+abhiishekpawar@users.noreply.github.com>
2021-06-24 08:37:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    var typed = new Typed('.typed', {
+        strings: ['Pythonist', 'web-developer', 'quick learner'],
+        smartBackspace: true,
+        loop: true,
+        typeSpeed: 100
+
+    })
+
+
+})
+
+function mail() {
+    location.replace("https://www.linkedin.com/in/abhishekpawar74/")
+}
+
+function res() {
+    location.replace("resume.pdf")
+}
